df2020f2ed484af9bd975389c09891f746ac99c6
Scott Lowe <scott.lowe@scottlowe.org>
2018-04-24 12:28:22 -0600

    Minor fixes to Terraform configuration
    Add version constraints for AWS provider (per terraform init). Add security groups for etcd nodes.
    
    Signed-off-by: Scott Lowe <scott.lowe@scottlowe.org>

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,48 @@ module "k8s-vpc" {
   subnet_map_pub_ip = "true"
 }
 
+resource "aws_security_group" "etcd_sg" {
+  name        = "etcd_sg"
+  description = "Allow traffic needed by etcd"
+  vpc_id      = "${module.k8s-vpc.vpc_id}"
+}
+
+resource "aws_security_group_rule" "etcd_sg_allow_sg_in" {
+  security_group_id        = "${aws_security_group.etcd_sg.id}"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = "${aws_security_group.etcd_sg.id}"
+}
+
+resource "aws_security_group_rule" "etcd_sg_allow_sg_out" {
+  security_group_id        = "${aws_security_group.etcd_sg.id}"
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = "${aws_security_group.etcd_sg.id}"
+}
+
+resource "aws_security_group_rule" "etcd_sg_allow_client" {
+  security_group_id = "${aws_security_group.etcd_sg.id}"
+  type              = "ingress"
+  from_port         = 2379
+  to_port           = 2379
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "etcd_sg_allow_peer" {
+  security_group_id = "${aws_security_group.etcd_sg.id}"
+  type              = "ingress"
+  from_port         = 2380
+  to_port           = 2380
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 module "etcd" {
   source = "./modules/instance-cluster"
 
@@ -18,7 +60,7 @@ module "etcd" {
   ssh_key        = "${var.key_pair}"
   cluster_size   = 3
   subnet_list    = ["${module.k8s-vpc.subnet_id}"]
-  sec_group_list = ["${module.k8s-vpc.default_sg_id}"]
+  sec_group_list = ["${module.k8s-vpc.default_sg_id}", "${aws_security_group.etcd_sg.id}"]
   role           = "etcd"
 }
 
